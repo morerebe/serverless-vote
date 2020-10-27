2437ae3fae662759cdfddc6094c87e00f2338e69
morerebe <59507960+morerebe@users.noreply.github.com>
2020-10-27 14:18:41 -0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,17 @@
+let positionArray = [0, 0];
+
+exports.handler = async event => {
+
+    const lat =  event.queryStringParameters.lat;
+    const lng =  event.queryStringParameters.lng;
+        positionArray[0] = lat;
+        positionArray[1] = lng;
+        
+        console.log(JSON.stringify(positionArray));
+
+        return {
+            statusCode: 200,
+            headers: { 'Access-Control-Allow-Origin' : '*'},
+            body: JSON.stringify(positionArray),
+        }
+  }
