1dfa8da57b8a5da7dfea85e0db243fbd6e598970
morerebe <59507960+morerebe@users.noreply.github.com>
2020-10-27 13:43:37 -0400

    Add files via upload

--- functions/hello.js
+++ functions/hello.js
@@ -1,29 +1,17 @@
-let votesArray = [0, 0, 0, 0];
+let positionArray = [0, 0];
 
 exports.handler = async event => {
 
-    const v =  event.queryStringParameters.vote
-        if (v === "a") {
-            votesArray[0] += 1;
-        }
-        else if (v === "b") {
-            votesArray[1] += 1;
-        }
-        else if (v === "c") {
-            votesArray[2] += 1;
-        }
-        else if (v === "d") {
-            votesArray[3] += 1;
-        }
-        else if (v === "r") {
-            votesArray = [0, 0, 0, 0];
-        }
+    const lat =  event.queryStringParameters.lat
+    const lng =  event.queryStringParameters.lat
+        positionArray[0] = lat;
+        positionArray[1] = lng;
         
-        console.log(JSON.stringify(votesArray));
+        console.log(JSON.stringify(positionArray));
 
         return {
             statusCode: 200,
             headers: { 'Access-Control-Allow-Origin' : '*'},
-            body: JSON.stringify(votesArray),
+            body: JSON.stringify(positionArray),
         }
   }
